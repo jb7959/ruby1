11488b7d201e2fba7fd736da74b9450ecbc7cf5b
Jerry_Ahn <jb7959@naver.com>
2015-03-25 09:29:26 +0000

    .

---
+++ textAnalizer.rb
@@ -0,0 +1,11 @@
+text=''
+line_count=0
+File.open("text.txt").each {|line| 
+    line_count += 1
+    text << line
+}
+total_characters = text.length
+puts "#{line_count} lines, #{total_characters} characters"
+
+total_characters_nospaces = text.gsub(/\s+/, '').length
+puts "#{total_characters_nospaces} total_characters_nospaces"
