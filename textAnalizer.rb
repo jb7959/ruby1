d2dec72cfd2a3463edbeb166beb42169a0174221
Jerry_Ahn <jb7959@naver.com>
2015-03-27 05:31:07 +0000

    .

--- textAnalizer.rb
+++ textAnalizer.rb
@@ -9,3 +9,4 @@ puts "#{line_count} lines, #{total_characters} characters"
 
 total_characters_nospaces = text.gsub(/\s+/, '').length
 puts "#{total_characters_nospaces} total_characters_nospaces"
+puts "This is a test #{text.scan(/\W+/).length}"
