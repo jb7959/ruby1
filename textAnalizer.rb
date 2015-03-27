1236d1be498221c563a05ad490987c9b34facb7a
Jerry_Ahn <jb7959@naver.com>
2015-03-27 05:45:34 +0000

    .

--- textAnalizer.rb
+++ textAnalizer.rb
@@ -8,5 +8,10 @@ total_characters = text.length
 puts "#{line_count} lines, #{total_characters} characters"
 
 total_characters_nospaces = text.gsub(/\s+/, '').length
+word_count = text.scan(/\W+/).length
+sentence_count =  text.split(/\.|\?|!/).length
+  
+
 puts "#{total_characters_nospaces} total_characters_nospaces"
-puts "This is a test #{text.scan(/\W+/).length}"
+puts "word_count #{word_count}"
+puts "sentence_count =  #{sentence_count}"
